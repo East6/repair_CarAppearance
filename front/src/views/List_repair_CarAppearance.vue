223e59d21cfc087838cf3a7204fed8f189a5c379
East6 <e165714@ie.u-ryukyu.ac.jp>
2022-04-12 14:10:07 +0900

    merge処理での失敗を修正

--- front/src/views/List_repair_CarAppearance.vue
+++ front/src/views/List_repair_CarAppearance.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="List_reapir_CarApperance">
     <h1>仮_板金塗装屋さん(車の外見の修理工場)の<b>一覧</b>ページ</h1>
+    <router-link to="/Detail_repair_CarApperance">修理工場<b>詳細</b>ページへ</router-link>
   </div>
 </template>
