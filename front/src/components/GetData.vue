151163cb301311d2412f30027ca5692bfc09372b
East6 <e165714@ie.u-ryukyu.ac.jp>
2022-04-28 17:48:52 +0900

    詳細ページに　仮のAPI取得用のコンポーネントの情報を反映

---
+++ front/src/components/GetData.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="getdata">
+    <!--<h2>{{ msg }}</h2>-->
+    <p>
+      コンポーネントの,GetDataからデータを取得中
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "GetData",
+    props: {
+    },
+  };
+</script>
--- front/src/views/Detail_repair_CarApperance.vue
+++ front/src/views/Detail_repair_CarApperance.vue
@@ -2,4 +2,18 @@
   <div class="detail_reapir_CarApperance">
     <h1>仮_板金塗装屋さん(車の外見の修理工場)の<b>詳細</b>ページ</h1>
   </div>
+  <br>
+  <GetData /><!-- 左のタグを入れたところに，コンポーネント情報が表示されるらしい-->
 </template>
+
+<script>
+import GetData from "@/components/GetData.vue";
+
+export default{
+  name: "Goal_get-JsonAPI",
+  components: {
+    GetData,
+  },
+};
+
+</script>
