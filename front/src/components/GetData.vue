9faa7fc63a331c5602ca2fd62b4536d8f8e2f30b
East6 <e165714@ie.u-ryukyu.ac.jp>
2022-06-01 18:38:46 +0900

    Axiosでローカルファイルのデータを取得(同期的に処理)し，表示

--- front/src/components/GetData.vue
+++ front/src/components/GetData.vue
@@ -3,14 +3,37 @@
     <!--<h2>{{ msg }}</h2>-->
     <p>
       コンポーネントの,GetDataからデータを取得中
+      <p>{{data.title}}</p>
+      <p>{{data.message}}</p>
+      <p>{{data.mydata}}</p>
     </p>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "GetData",
-    props: {
+  import axios from "axios"
+  import { reactive, onMounted } from "vue"
+
+  const url = "/test_data.txt"
+
+  export default{
+    setup(props){
+      //data変数?にそれぞれ値を入れている(data.titleは「Axios」が表示)
+      const data = reactive({
+        title:"Axios",
+        message:"this is axios sample.",
+        mydata: "",
+      })
+      // axiosを用いて，データを取得している
+      const getData = async()=>{
+        let result = await axios.get(url)
+        data.mydata = result.data
+      }
+      //OnMoutedは，コンポーネントがマウント(Detail_repair_CarApperance.vueで表示)された時に呼び出される，イベント
+      onMounted(()=> {
+        getData()
+      })
+      return { data, getData}
     },
-  };
+  }
 </script>
