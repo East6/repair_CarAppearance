056c52c7fff48095141e5f2612d76f84896c7760
East6 <e165714@ie.u-ryukyu.ac.jp>
2022-06-02 15:09:48 +0900

    Axiosでリモートデータ(json型)を取得して，表示

--- front/src/components/GetData.vue
+++ front/src/components/GetData.vue
@@ -5,7 +5,29 @@
       コンポーネントの,GetDataからデータを取得中
       <p>{{data.title}}</p>
       <p>{{data.message}}</p>
-      <p>{{data.mydata}}</p>
+      <br>
+      <p>{{data.json_data}}</p>
+      上記のjsonデータを綺麗にすると以下のようになる<br>
+      <table>
+        <tr>
+          <th>ID</th>
+          <td>{{data.json_data ? data.json_data.id : "-"}} </td><!--何この書き方?!-->
+        </tr>
+
+        <tr>
+          <th>Title</th>
+          <td>{{data.json_data ? data.json_data.title : "-"}}</td>
+        </tr>
+
+        <tr>
+          <th>Body</th>
+          <td>{{data.json_data ? data.json_data.body : "-" }}</td>
+        </tr>
+
+
+      </table>
+     
+
     </p>
   </div>
 </template>
@@ -14,7 +36,7 @@
   import axios from "axios"
   import { reactive, onMounted } from "vue"
 
-  const url = "/test_data.txt"
+  const url = "https://jsonplaceholder.typicode.com/posts/"
 
   export default{
     setup(props){
@@ -22,13 +44,17 @@
       const data = reactive({
         title:"Axios",
         message:"this is axios sample.",
-        mydata: "",
+        json_data: null,
       })
       // axiosを用いて，データを取得している(非同期処理)
       const getData = ()=>{
-        //getの後に，thenを呼び出す(=>の後の内容が実行)らしい...
-        axios.get(url).then((result)=> {
-          data.mydata = result.data
+        let id = 1 // jsonのidの番号
+        //getでカッコ内のデータを受け取った後に，thenを呼び出す(=>の後の内容が実行)らしい...
+        //axiosでデータを受け取ると，json形式の場合，オブジェクトに変換してくれるらしい...
+        /// data.json_data というオブジェクトの考え方????
+        axios.get(url + id).then((result)=> {
+          console.log(result.data)
+          data.json_data = result.data
         })
       }
       //OnMoutedは，コンポーネントがマウント(Detail_repair_CarApperance.vueで表示)された時に呼び出される，イベント
