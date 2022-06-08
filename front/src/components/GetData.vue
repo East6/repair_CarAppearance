e1d03e2ba106fb75cc0064cf32372714776dfbd8
East6 <e165714@ie.u-ryukyu.ac.jp>
2022-06-08 17:38:24 +0900

    Axiosでリモートデータ(json型)を取得して，表示(commitし忘れ?)

--- front/src/components/GetData.vue
+++ front/src/components/GetData.vue
@@ -7,6 +7,12 @@
       <p>{{data.message}}</p>
       <br>
       <p>{{data.json_data}}</p>
+
+      <div class="formgroup">
+        <input type="number" v-model="data.id" /><!--v-modelの中に入れたのを引き継いで，doClickの処理ができるっぽい-->
+        <button @click="doClick">Click</button>
+      </div>
+
       上記のjsonデータを綺麗にすると以下のようになる<br>
       <table>
         <tr>
@@ -44,24 +50,21 @@
       const data = reactive({
         title:"Axios",
         message:"this is axios sample.",
+        id:0,
         json_data: null,
       })
-      // axiosを用いて，データを取得している(非同期処理)
-      const getData = ()=>{
-        let id = 1 // jsonのidの番号
+      // axiosを用いて，データを取得している(非同期処理)_clickした時の処理を記述
+      const doClick = ()=>{
+
         //getでカッコ内のデータを受け取った後に，thenを呼び出す(=>の後の内容が実行)らしい...
         //axiosでデータを受け取ると，json形式の場合，オブジェクトに変換してくれるらしい...
         /// data.json_data というオブジェクトの考え方????
-        axios.get(url + id).then((result)=> {
+        axios.get(url + data.id).then((result)=> {
           console.log(result.data)
           data.json_data = result.data
         })
       }
-      //OnMoutedは，コンポーネントがマウント(Detail_repair_CarApperance.vueで表示)された時に呼び出される，イベント
-      onMounted(()=> {
-        getData()
-      })
-      return { data, getData}
+      return { data, doClick}
     },
   }
 </script>
